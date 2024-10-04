447f4ead8c46cdcfb0658ffd71d6619a6a43b790
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-10-04 21:18:09 +0300

    create posts service

---
+++ services/posts.ts
@@ -0,0 +1,7 @@
+const CreatePost = async() => {}
+const FetchPosts = async () => {}
+const UpdatePost = async () => {}
+const DeletePost = async() => {}
+
+
+export { CreatePost, FetchPosts, UpdatePost, DeletePost }
