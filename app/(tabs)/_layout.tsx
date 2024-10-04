44a552f371fcf504394d5d72c96195cdb4d5eab8
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-10-04 21:29:54 +0300

    register new tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,36 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import {Feather} from "@expo/vector-icons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabsLayout: React.FC = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
+          name="index"
+          options={{
+              title: 'Home',
+              tabBarIcon: ({ color, focused }) => (
+                  <Feather name="home" size={24} color={color} focused={focused} />
+              )}}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+        <Tabs.Screen name="forum" options={{
+              title: 'Community',
+              tabBarIcon: ({ color, focused }) => (
+                   <MaterialCommunityIcons name="account-group" size={24} color={color} focused={focused} />
+              )}} />
+      <Tabs.Screen name="search" options={{
+              title: 'Search',
+              tabBarIcon: ({ color, focused }) => (
+                  <Feather name="search" size={24} color={color} focused={focused} />
+              )}} />
+        <Tabs.Screen name="messages" options={{
+              title: 'Messages',
+              tabBarIcon: ({ color, focused }) => (
+                  <Feather name="mail" size={24} color={color} focused={focused} />
+              )}} />
     </Tabs>
   );
-}
+};
+
+export default TabsLayout;
