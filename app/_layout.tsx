e960109816fea720289790cb3d0dc42363649398
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-10-24 20:36:25 +0300

    refactor root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,21 +6,21 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import {TamaguiProvider} from "tamagui";
 import config from "@/tamagui.config";
-import {ChatProvider} from "@/context/ChatContext";
+
+
+
 
 const RootLayout: React.FC = () => {
   return (
       <GestureHandlerRootView style={{ flex: 1 }}>
           <BottomSheetModalProvider>
-            <AuthProvider>
-                <ChatProvider>
+                <AuthProvider>
                     <TamaguiProvider config={config}>
-                      <View style={{ flex: 1 }}>
-                        <Slot />
-                      </View>
+                          <View style={{ flex: 1 }}>
+                            <Slot />
+                          </View>
                     </TamaguiProvider>
-                </ChatProvider>
-            </AuthProvider>
+                </AuthProvider>
           </BottomSheetModalProvider>
       </GestureHandlerRootView>
   );
