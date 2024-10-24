7ff7d6ffd5da1808b0dbcf4c4b926fe891cda4f5
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-10-24 20:35:44 +0300

    create stream client

--- utils/Stream.ts
+++ utils/Stream.ts
@@ -9,6 +9,9 @@ interface StreamUser {
     user_token?: string;
 }
 
+export const streamAppId='ckz7av5jj9n4';
+export const streamAppSecret= 'r4z9bg4uerb2hjgpnn54vc7sa8e4xz2b7mp3qke2zs6mhu2mnpcrcngyxpbfywud'
+
 export const chatApiKey = 'dz5f4d5kzrue'
 export const chatUserId = 'lively-wood-7';
 export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGl2ZWx5LXdvb2QtNyIsImV4cCI6MTcxMDUzMjQ0Nn0.KMkjl68QocLTO_B5E4jIw2s5VBmRLOIvFnZkYNQepmc';
@@ -41,4 +44,4 @@ const createChannel = async (channelName: string, users: [], channelId: string) 
     }
 }
 
-const sendMessage = async ()
+// const sendMessage = async ()
